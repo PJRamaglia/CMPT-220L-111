9aefe99287b1dcabedc862a650da9812d7d16374
Peter J. Ramaglia <peter.j.ramaglia@outlook.com>
2014-12-11 01:18:17 -0500

    Project Update

---
+++ Projects/src/edu/marist/cmpt220l/project02/Olympian.java
@@ -0,0 +1,14 @@
+package edu.marist.cmpt220l.project02;
+
+/**
+ * Created by Peter J. Ramaglia on 12/10/2014.
+ */
+public class Olympian {
+    String Name;
+    Gender Gender;
+    int Age;
+
+    public enum Gender {
+        Male, Female
+    }
+}
